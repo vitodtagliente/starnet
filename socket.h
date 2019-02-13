d98308e4a32f111c03f4c2dbf121f2ccd0a6af05
Vito Domenico Tagliente <hackjoker@hotmail.it>
2019-02-13 13:59:42 +0100

    changes

--- socket.h
+++ socket.h
@@ -16,57 +16,56 @@ namespace starnet
 			UDP
 		};
 
-		Socket(const NetAddress& address, const Type type = Type::UDP)
-			: m_address{ address }, m_type{ type }
-		{
-			m_socket = socket(
-				address.getFamily(),
-				(m_type == Type::UDP) ? SOCK_DGRAM : SOCK_STREAM,
-				(m_type == Type::UDP) ? IPPROTO_UDP : IPPROTO_TCP
-			);
+		Socket(const Type type = Type::UDP)
+			: m_socket{ INVALID_SOCKET }, m_type{ type }
+		{ 
+		
 		}
 
 		Socket(const Socket& socket)
 		{
 			m_socket = socket.m_socket;
-			m_address = socket.m_address;
 			m_type = socket.m_type;
 		}
 
 		virtual ~Socket() = default;
 
 		inline const SOCKET& getSocket() const { return m_socket; }
-		inline const NetAddress& getAddress() const { return m_address; }
 		inline Type getType() const { return m_type; }
 
 		inline bool isValid() const { return m_socket != INVALID_SOCKET; }
 
-		bool operator== (const Socket& other) const = delete;
-		bool operator!= (const Socket& other) const = delete;
+		bool operator== (const Socket& other) const { return m_socket == other.m_socket; }
+		bool operator!= (const Socket& other) const { return !(*this == other); }
 
-	private:
-
-		/*
-		inline bool bind()
+		inline bool bind(const NetAddress& address)
 		{
-			if (::bind(0, &m_address.getSocketAddress(), m_address.getSize()) != BindSuccess)
+			if (::bind(0, &address.getSocketAddress(), address.getSize()) != BindSuccess)
 			{
 				// #todo: error management
 				return false;
 			}
 			return true;
 		}
-		*/
+
+	private:
+
+		inline void initialize(const NetAddress::AddressFamily family)
+		{
+			m_socket = socket(
+				family,
+				(m_type == Type::UDP) ? SOCK_DGRAM : SOCK_STREAM,
+				(m_type == Type::UDP) ? IPPROTO_UDP : IPPROTO_TCP
+			);
+		}
 
 		// socket data
 		SOCKET m_socket;
-		// socket address
-		NetAddress m_address;
 		// socket type
 		Type m_type;
 
-		//static const int BindSuccess;
+		static const int BindSuccess;
 	};
 
-	//const int Socket::BindSuccess{ 0 };
+	const int Socket::BindSuccess{ 0 };
 }
