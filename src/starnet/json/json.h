d4284cee302a33856dca0bb4966237b7b1cb70ca
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2019-11-20 21:23:48 +0100

    changes

--- src/starnet/json/json.h
+++ src/starnet/json/json.h
@@ -8,9 +8,13 @@ namespace starnet
 {
 	namespace json
 	{
-		class Value
+		class data
 		{
 		public:
+
+			using string_t = std::string;
+			using array_t = std::vector<data>;
+			using object_t = std::map<string_t, data>;
 
 			enum class Type
 			{
@@ -22,121 +26,201 @@ namespace starnet
 				String
 			};
 
-			Value()
+			data()
 				: m_type(Type::Null)
-				, m_value()
+				, m_numberType(NumberType::Undefined)
+				, m_bool()
 			{}
 
-			Value(const bool value)
+			data(const bool value)
 				: m_type(Type::Boolean)
-				, m_value(std::to_string(value))
+				, m_numberType(NumberType::Undefined)
+				, m_bool(value)
 			{}
 
-			Value(const int value)
+			data(const int value)
 				: m_type(Type::Number)
-				, m_value(std::to_string(value))
+				, m_numberType(NumberType::Int)
+				, m_int(value)
 			{}
 
-			Value(const double value)
+			data(const double value)
 				: m_type(Type::Number)
-				, m_value(std::to_string(value))
+				, m_numberType(NumberType::Double)
+				, m_double(value)
 			{}
 
-			Value(const float value)
+			data(const float value)
 				: m_type(Type::Number)
-				, m_value(std::to_string(value))
+				, m_numberType(NumberType::Float)
+				, m_float(value)
 			{}
 
-			Value(const std::string& value)
+			data(const string_t& value)
 				: m_type(Type::String)
-				, m_value(value)
+				, m_numberType(NumberType::Undefined)
+				, m_string(value)
 			{}
 
-			Value(const std::vector<Value>& values)
+			data(const array_t& value)
 				: m_type(Type::Array)
-				, m_values(values)
+				, m_numberType(NumberType::Undefined)
+				, m_array(value)
+			{}
+
+			data(const object_t& value)
+				: m_type(Type::Object)
+				, m_numberType(NumberType::Undefined)
+				, m_object(value)
 			{}
 
 			bool is_array() const { return m_type == Type::Array; }
 			bool is_bool() const { return m_type == Type::Boolean; }
-			bool is_null() const { return m_type == Type::Null || m_value.length() == 0; }
+			bool is_null() const { return m_type == Type::Null; }
 			bool is_number() const { return m_type == Type::Number; }
 			bool is_object() const { return m_type == Type::Object; }
 			bool is_string() const { return m_type == Type::String; }
 
-			bool as_bool() const { return static_cast<bool>(std::atoi(m_value.c_str())); }
-			int as_int() const { return std::atoi(m_value.c_str()); }
-			float as_float() const { return std::atof(m_value.c_str()); }
-			double as_double() const { return std::stod(m_value.c_str()); }
-			const std::string& as_string() const { return m_value; }
+			bool as_bool() const { return m_bool; }
+			int as_int() const { return m_int; }
+			float as_float() const { return m_float; }
+			double as_double() const { return m_double; }
+			const array_t& as_array() const { return m_array; }
+			const object_t& ar_object() const { return m_object; }
+			const string_t& as_string() const { return m_string; }
 
-			std::string to_string() const { return m_value; }
+			Type type() const { return m_type; }
 
-			bool operator== (const Value& other) const
-			{
-				return m_type == other.m_type
-					&& m_value == other.m_value;
+			string_t to_string() const 
+			{ 
+				switch (m_type)
+				{
+				case Type::Boolean: return std::to_string(m_bool);
+				case Type::Null: return "";
+				case Type::Array: return "";
+				case Type::Object: return "";
+				case Type::Number:
+				{
+					switch (m_numberType)
+					{
+					case NumberType::Int: return std::to_string(m_int);
+					case NumberType::Double: return std::to_string(m_double);
+					case NumberType::Float: return std::to_string(m_float);
+					default: return "";
+					}
+				}
+				default:
+					return m_string;
+				}
 			}
 
-			bool operator!= (const Value& other) const
+			bool operator== (const data& other) const
 			{
-				return m_type != other.m_type
-					|| m_value != other.m_value;
+				if (m_type == other.m_type)
+				{
+					switch (m_type)
+					{
+					case Type::Boolean: return m_bool == other.m_bool;
+					case Type::Null: return true;
+					case Type::String: return m_string == other.m_string;
+					/// #TODO
+					}
+				}
+				return false;
 			}
 
-			Value& operator= (const Value& other)
+			bool operator!= (const data& other) const
 			{
-				m_type = other.m_type;
-				m_value = other.m_value;
+				return true; /// TODO
+			}
+
+			data& operator= (const data& other)
+			{
+				
 
 				return *this;
 			}
 
-			Value& operator= (const bool value)
+			data& operator= (const bool value)
 			{
-				m_type = Type::Boolean;
+				
 
 				return *this;
 			}
 
-			Value& operator= (const int value)
+			data& operator= (const int value)
 			{
 				m_type = Type::Number;
+				m_int = value;
 
 				return *this;
 			}
 
-			Value& operator= (const double value)
+			data& operator= (const double value)
 			{
 				m_type = Type::Number;
+				m_numberType = NumberType::Double;
+				m_double = value;
 
 				return *this;
 			}
 
-			Value& operator= (const float value)
+			data& operator= (const float value)
 			{
 				m_type = Type::Number;
+				m_numberType = NumberType::Float;
+				m_float = value;
 
 				return *this;
 			}
 
-			Value& operator= (const std::string& value)
+			data& operator= (const std::string& value)
 			{
 				m_type = Type::String;
+				m_string = value;
+
+				return *this;
+			}
+
+			data& operator= (const array_t& value)
+			{
+				m_type = Type::Array;
+				m_array = value;
+
+				return *this;
+			}
+
+			data& operator= (const object_t& value)
+			{
+				m_type = Type::Object;
+				m_object = value;
 
 				return *this;
 			}
 
 		private:
 
+			enum class NumberType
+			{
+				Undefined,
+				Int,
+				Float,
+				Double
+			};
+
 			// value type
 			Type m_type;
+			NumberType m_numberType;
 			// raw data
 			union
 			{
-				std::string m_value;
-				std::vector<Value> m_values;
-				std::map<std::string, Value> m_propeties;
+				bool m_bool;
+				int m_int;
+				float m_float;
+				double m_double;
+				std::string m_string;
+				std::vector<data> m_array;
+				std::map<std::string, data> m_object;
 			};
 		};
 	}
